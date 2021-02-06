0dd21a9e008890de20808e2e4080e1bb03e43b7c
DaisukeMatsuura <mail@brightful.jp>
2021-02-06 23:07:38 +0900

    Favorite マイグレーションファイル作成

---
+++ database/migrations/2021_02_04_163612_create_favorites_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->string('track');
+            $table->string('album');
+            $table->string('artist');
+            $table->string('image_path');
+            $table->date('release_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
