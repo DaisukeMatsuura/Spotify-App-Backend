55d54fcb544a9877fa539c6c4f2beccc067c7c21
DaisukeMatsuura <mail@brightful.jp>
2021-02-08 10:27:42 +0900

    Favorite リソース＆コレクション作成

---
+++ app/Http/Resources/Favorite.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Http\Resources;
+
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Favorite extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => [
+                'type' => 'favorites',
+                'favorite_id' => $this->id,
+                'attributes' => [
+                    'track'        => $this->track,
+                    'album'        => $this->album,
+                    'artist'       => $this->artist,
+                    'image_path'   => $this->image_path,
+                    'release_date' => $this->release_date,
+                ]
+            ],
+            'links' => [
+                'self' => url('/api/favorites/'.$this->id),
+            ]
+        ];
+    }
+}
